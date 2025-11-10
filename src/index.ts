84f79cd52969fb7ee91b360649656866acdc58e8
gx8z <86722990+gx8z@users.noreply.github.com>
2025-11-10 19:15:20 +0700

    add routes and basic auth and octokit middleware to hono

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,62 @@
 import { Hono } from "hono";
+import { basicAuth } from "hono/basic-auth";
+import { Octokit } from "octokit";
+import infoFile from "../info.json" assert { type: "json" };
 
-const app = new Hono<{ Bindings: CloudflareBindings }>();
+const CONFIG = { owner: infoFile.owner, repo: infoFile.repo };
 
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+const app = new Hono<{
+	Bindings: CloudflareBindings;
+	Variables: {
+		octokit: Octokit;
+	};
+}>();
+
+app.use(
+	"/*",
+	// Basic Auth Middleware
+	async (c, next) => {
+		const auth = basicAuth({
+			username: c.env.WEB_USERNAME,
+			password: c.env.WEB_PASSWORD
+		});
+		return auth(c, next);
+	},
+	// Octokit Middleware
+	async (c, next) => {
+		if (c.get("octokit")) {
+			return await next();
+		}
+		const token = c.env.GH_TOKEN;
+		if (!token) {
+			console.error("GH_TOKEN is not set in Cloudflare environment");
+			return c.json({ error: "Server configuration error" }, 500);
+		}
+
+		const octokit = new Octokit({
+			auth: token
+		});
+		c.set("octokit", octokit);
+		await next();
+	}
+);
+
+app.get("/download/:id/:name?", async (c) => {
+	const { id } = c.req.param();
+	const octokit = c.get("octokit");
+
+	const assets = await octokit.request(
+		"HEAD /repos/{owner}/{repo}/releases/assets/{asset_id}",
+		{
+			...CONFIG,
+			asset_id: +id,
+			headers: {
+				accept: "application/octet-stream"
+			}
+		}
+	);
+
+	if (assets.url) return c.redirect(assets.url);
 });
 
 export default app;
