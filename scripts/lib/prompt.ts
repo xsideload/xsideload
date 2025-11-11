a604da564c4df7fbd8023b4349eb9883a2a243f8
gx8z <86722990+gx8z@users.noreply.github.com>
2025-11-11 23:20:10 +0700

    add path and prompt helpers

---
+++ scripts/lib/prompt.ts
@@ -0,0 +1,22 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+export const prompt = async (question: string) => {
+	let answer: string;
+	const rl = readline.createInterface({ input, output });
+	try {
+		answer = await rl.question(question);
+	} catch (error) {
+		if ((error as any).code === "ABORT_ERR") {
+			console.log("Exiting");
+		} else {
+			console.error(error);
+		}
+		process.exit();
+	} finally {
+		rl.close();
+	}
+	return answer;
+};
+
+export const confirm = async () => await prompt("Continue? ");
