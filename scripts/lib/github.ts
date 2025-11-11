032a078ef7fb0271ea84afcf8661f4a50e96057b
gx8z <86722990+gx8z@users.noreply.github.com>
2025-11-11 22:25:52 +0700

    add github octokit helper

---
+++ scripts/lib/github.ts
@@ -0,0 +1,6 @@
+import { Octokit } from "octokit";
+
+const GH_TOKEN = process.env.GH_TOKEN;
+if (!GH_TOKEN) throw new Error("GH_TOKEN missing");
+
+export const octokit = new Octokit({ auth: GH_TOKEN });
