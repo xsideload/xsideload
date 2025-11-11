62e5248bd33e67f040f37b06a8fdf19ddabcf23c
gx8z <86722990+gx8z@users.noreply.github.com>
2025-11-11 23:36:03 +0700

    add uploadPrebuilt helper

---
+++ scripts/uploadPrebuilt/index.ts
@@ -0,0 +1,62 @@
+import {
+	assetsRepo,
+	getAppInfoFromTweakName,
+	type TweakName
+} from "@/scripts/lib/info";
+import { getOneArg } from "@/scripts/lib/args";
+import { resolvePath, basename } from "@/scripts/lib/path";
+import { confirm } from "@/scripts/lib/prompt";
+import { getIpaVersionFilePath } from "@/scripts/lib/getIpaVersion";
+import { octokit } from "@/scripts/lib/github";
+import { readFileSync } from "node:fs";
+
+export const uploadPrebuilt = async ({
+	tweakName,
+	optionalNotes,
+	getTweakVersion,
+	getAppVersion
+}: {
+	tweakName: TweakName;
+	optionalNotes?: string;
+	getTweakVersion?: ({ fileName }: { fileName: string }) => string;
+	getAppVersion?: ({ fileName }: { fileName: string }) => string;
+}) => {
+	const oneArg = getOneArg();
+	const filePath = resolvePath(oneArg);
+	const appVersion = getAppVersion
+		? getAppVersion({ fileName: basename(filePath) })
+		: await getIpaVersionFilePath(filePath);
+	console.log("- appVersion:", appVersion);
+	const tweakVersion = getTweakVersion
+		? getTweakVersion({ fileName: basename(filePath) })
+		: "0";
+	console.log("- tweakVersion:", tweakVersion);
+	const appName = getAppInfoFromTweakName(tweakName)?.name;
+	const tagName = optionalNotes
+		? `${appName}_${appVersion}_${tweakName}_${tweakVersion}_${optionalNotes}`
+		: `${appName}_${appVersion}_${tweakName}_${tweakVersion}`;
+	const newFileName = `${tagName}.ipa`;
+
+	console.log("- newFileName:", newFileName);
+	console.log("- Creating release", tagName);
+	console.log("- Uploading", filePath);
+	console.log();
+	await confirm();
+
+	const release = await octokit.rest.repos.createRelease({
+		...assetsRepo,
+		tag_name: tagName,
+		make_latest: "false"
+	});
+
+	if (!release.data.id) throw new Error("createRelease error");
+
+	const asset = await octokit.rest.repos.uploadReleaseAsset({
+		...assetsRepo,
+		release_id: release.data.id,
+		name: newFileName,
+		data: readFileSync(filePath) as unknown as string
+	});
+
+	console.log(`Uploaded ${asset.data.name} to ${release.data.html_url}`);
+};
