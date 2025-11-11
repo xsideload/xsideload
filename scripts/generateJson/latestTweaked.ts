4e00f00e43a311b6e48c3c85a01c83cb14840fe7
gx8z <86722990+gx8z@users.noreply.github.com>
2025-11-11 22:26:05 +0700

    add generateJson for latestTweaked

---
+++ scripts/generateJson/latestTweaked.ts
@@ -0,0 +1,70 @@
+import {
+	assetsRepo,
+	getAppInfoFromTweakName,
+	TweakName
+} from "@/scripts/lib/info";
+import { parseReleaseTag } from "@/scripts/lib/release";
+import { octokit } from "@/scripts/lib/github";
+import { BASE_URL, BASE_URL_WITH_BASIC_AUTH } from "@/scripts/lib/url";
+import { projectRoot } from "@/scripts/lib/path";
+import { aNewerThanB } from "@/scripts/lib/compare";
+import { writeFile } from "node:fs/promises";
+
+const limit: number | null = null;
+
+const result: { [key: string]: AppsJson } = {};
+
+const releasesIterator = octokit.paginate.iterator(
+	octokit.rest.repos.listReleases,
+	{
+		...assetsRepo,
+		per_page: 100
+	}
+);
+
+let releaseLoopCount = 0;
+releaseLoop: for await (const { data: pageOfReleases } of releasesIterator) {
+	for (const release of pageOfReleases) {
+		if (limit !== null && releaseLoopCount >= limit) {
+			break releaseLoop;
+		}
+		releaseLoopCount++;
+		// Check each release
+		const releaseInfo = parseReleaseTag(release.tag_name);
+		if (releaseInfo.type === "tweaked" && releaseInfo.tweakName) {
+			const appInfo = getAppInfoFromTweakName(
+				releaseInfo.tweakName as TweakName
+			);
+			const asset = release.assets[0];
+			if (
+				asset &&
+				aNewerThanB(
+					releaseInfo.appVersion,
+					result[releaseInfo.tweakName].version
+				)
+			) {
+				result[releaseInfo.tweakName] = {
+					name: releaseInfo.tweakName,
+					bundleIdentifier: appInfo?.bundleIdentifier || "",
+					version: `${releaseInfo.appVersion}_${releaseInfo.tweakVersion}`,
+					localizedDescription: asset.name,
+					downloadURL: `${BASE_URL_WITH_BASIC_AUTH}/download/${asset.id}/${asset.name}`,
+					iconURL: appInfo?.bundleIdentifier
+						? `${BASE_URL}/icon/${appInfo?.bundleIdentifier}.png`
+						: "",
+					versionDate: asset.created_at,
+					size: asset.size
+				};
+			}
+		}
+	}
+}
+
+try {
+	await writeFile(
+		`${projectRoot}/generated/repo.json`,
+		JSON.stringify(Object.values(result))
+	);
+} catch (error) {
+	console.error("Error writing JSON to file:", error);
+}
---
+++ scripts/generateJson/types.ts
@@ -0,0 +1,10 @@
+type AppsJson = {
+	name: string | null;
+	bundleIdentifier: string;
+	version: string;
+	localizedDescription: string;
+	downloadURL: string;
+	iconURL: string;
+	versionDate: string;
+	size: number;
+};
